cbec002674763433e8d741f97fcdaf2d6a426ae8
KurumiWWW <2209278137@qq.com>
2023-05-19 01:42:31 +0800

    init style, added vue-router, pinia

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="app">
+    <div class="app-item"></div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import "@/assets/style/reset";
+.app {
+  &-item {
+    width: 100px;
+    height: 100px;
+    background-image: url("@/assets/logo.png");
+  }
 }
 </style>
