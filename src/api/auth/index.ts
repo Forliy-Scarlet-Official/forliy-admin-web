944869089d5b5cbbd92c8b8cdc961021d90cdc0c
kurumiwww <2209278137@qq.com>
2023-11-08 19:04:39 +0800

    更新依赖

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,9 +1,25 @@
 import API from "@/utils/request";
 
+interface LoginRes {
+  user: {
+    id: number;
+    username: string;
+    email: string;
+    status: number;
+    createdDate: string;
+  };
+  token: string;
+  tokenVersion: number;
+  tokenExpireAt: string | null;
+}
+
 export default {
   // 用户登录
-  login: (req: { type: string; email: string; password: string }) =>
-    API.post("/auth/login", req),
+  login: (req: {
+    type: string;
+    email: string;
+    password: string;
+  }): Promise<LoginRes> => API.post("/auth/login", req),
   // 用户注册
   signup: (req: { username: string; email: string; password: string }) =>
     API.post("/auth/signup", req),
